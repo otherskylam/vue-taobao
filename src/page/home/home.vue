f7ff6534b742a56eb45adadf8e802fb796f9ee2c
otherskylam <185924704@qq.com>
2021-01-23 08:21:14 +0800

    use vue-router

---
+++ src/page/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+<header>
+  <div class="home_header"></div>
+</header>
+</template>
+
+<script>
+export default {
+name: "home"
+}
+</script>
+
+<style scoped>
+.home_header {
+  width: 100%;
+  height: 37px;
+  background-color: #FF852A;
+}
+</style>
