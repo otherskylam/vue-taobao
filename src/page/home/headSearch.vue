d30020074fd9ab368a8f82315d942ecb4c86ccdd
otherskylam <185924704@qq.com>
2021-03-24 16:15:00 +0800

    update searchBox adjust width

--- src/page/home/headSearch.vue
+++ src/page/home/headSearch.vue
@@ -1,15 +1,16 @@
 <template>
-<!--  <header>-->
-    <div class="home_header">
-      <!--      <input class="searchBox" type="text" placeholder="寻找宝贝店铺">-->
-      <a class="searchBox" @click="goSearch">
-        <svg class="icon icon_search" aria-hidden="true">
-          <use xlink:href="#icon-sousuo"></use>
-        </svg>
-        寻找宝贝店铺
-      </a>
-    </div>
-<!--  </header>-->
+  <!--  <header>-->
+  <div class="home_header"
+       :style="{paddingTop: style.homeHeader.padding.top, paddingRight: style.homeHeader.padding.right, paddingBottom: style.homeHeader.padding.bottom, paddingLeft: style.homeHeader.padding.left}">
+    <!--      <input class="searchBox" type="text" placeholder="寻找宝贝店铺">-->
+    <a class="searchBox" :style="{width: style.searchBox.width}" @click="goSearch">
+      <svg class="icon icon_search" aria-hidden="true">
+        <use xlink:href="#icon-sousuo"></use>
+      </svg>
+      寻找宝贝店铺
+    </a>
+  </div>
+  <!--  </header>-->
 </template>
 
 <script>
@@ -18,6 +19,16 @@ import router from "@/router";
 
 export default {
   name: "headSearch",
+  data() {
+    return {
+      style: {searchBox: {width: '0px'}, homeHeader: {padding: {top: '6px', right: '9px', bottom: '6px', left: '42px'}}}
+    }
+  },
+  mounted() {
+    //TODO 考虑优化，这样写padding很不好
+    let paddingData = parseInt(this.style.homeHeader.padding.left) + parseInt(this.style.homeHeader.padding.right);
+    this.style.searchBox.width = window.innerWidth - paddingData + 'px';
+  },
   methods: {
     goSearch: function () {
       router.push('search');
@@ -37,11 +48,11 @@ export default {
     size: 27px 20px;
     position: 9px center;
   };
-  padding: 6px 9px 6px 42px;
+  //padding: 6px 9px 6px 42px;
 }
 
 .searchBox {
-  @include wh(324px, 25px);
+  height: 25px;
   display: block;
   border-radius: 4px;
   background: #ff4e22;
