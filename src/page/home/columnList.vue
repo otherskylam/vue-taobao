5735c20824b6163545555116bf4dfdd93adbb8c8
otherskylam <185924704@qq.com>
2021-03-26 16:20:17 +0800

    update columnList.vue

--- src/page/home/columnList.vue
+++ src/page/home/columnList.vue
@@ -3,21 +3,96 @@
     <div class="columnList" :style="{height: columnListHeight()}">
       <div class="columnListLeft">
         <div class="leftFirst">
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div>
+          <div class="leftContainer leftFirstLeft">
+            <div class="leftContainerTitle"></div>
+            <div class="leftContainerImg">
               <span>品牌折扣</span>
               <div></div>
             </div>
           </div>
-          <div></div>
+          <div class="leftContainer leftFirstRight">
+            <div class="leftContainerTitle"></div>
+            <div class="leftContainerImg">
+              <span>划算好货</span>
+              <div></div>
+            </div>
+          </div>
         </div>
-        <div class="leftSecond"></div>
-        <div class="leftThird"></div>
+        <div class="leftSecond">
+          <div class="leftContainer leftSecondLeft">
+            <div class="leftContainerTitle"></div>
+            <div class="leftContainerImg">
+              <span>限时半价</span>
+              <div></div>
+            </div>
+          </div>
+          <div class="leftContainer leftSecondRight">
+            <div class="leftContainerTitle"></div>
+            <div class="leftContainerImg">
+              <span>9.9包邮</span>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div class="containerB leftThird">
+          <div class="containerBTitle leftThirdTitle"></div>
+          <div class="containerBLeft leftThirdLeft">
+            <div class="containerBLeftBox leftThirdLeftBox">
+              <span>挖深藏的店</span>
+              <div></div>
+            </div>
+          </div>
+          <div class="containerBRight leftThirdRight">
+            <div></div>
+          </div>
+        </div>
       </div>
-      <div class="columnListRight"></div>
+      <div class="columnListRight">
+        <div class="containerB" v-for="(item, index) in containerB" :key="index">
+          <div class="containerBTitle" :style="{backgroundImage: 'url(' + require('../../image/' + item.title.imgName) + ')'}"></div>
+          <div class="containerBLeft">
+            <div class="containerBLeftBox">
+              <span :style="{color: item.secTitle.color}">{{ item.secTitle.data }}</span>
+              <div :style="{backgroundImage: 'url(' + require('../../image/' + item.leftImg.name) + ')'}"></div>
+            </div>
+          </div>
+          <div class="containerBRight">
+            <div :style="{backgroundImage: 'url(' + require('../../image/' + item.rightImg.name) + ')'}"></div>
+          </div>
+        </div>
+      </div>
+      <!--      <div class="columnListRight">-->
+      <!--        <div class="containerB">-->
+      <!--          <div class="containerBTitle"></div>-->
+      <!--          <div class="containerBLeft">-->
+      <!--            <span>好物传送门</span>-->
+      <!--            <div></div>-->
+      <!--          </div>-->
+      <!--          <div class="containerBRight">-->
+      <!--            <div></div>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--        <div class="containerB">-->
+      <!--          <div class="containerBTitle"></div>-->
+      <!--          <div class="containerBLeft">-->
+      <!--            <span>好物传送门</span>-->
+      <!--            <div></div>-->
+      <!--          </div>-->
+      <!--          <div class="containerBRight">-->
+      <!--            <div></div>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--        <div class="containerB">-->
+      <!--          <div class="containerBTitle"></div>-->
+      <!--          <div class="containerBLeft">-->
+      <!--            <span>好物传送门</span>-->
+      <!--            <div></div>-->
+      <!--          </div>-->
+      <!--          <div class="containerBRight">-->
+      <!--            <div></div>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--      </div>-->
     </div>
   </div>
 </template>
@@ -27,6 +102,27 @@ export default {
   name: "columnList",
   data() {
     return {
+      baseUrl: '../../image/',
+      containerB: [
+        {
+          title: {imgName: 'columnList_tbzb.png'},
+          secTitle: {data: '好物传送门', color: '#ff507d'},
+          leftImg: {name: 'columnList_hwcsmimg1.png'},
+          rightImg: {name: 'columnList_hwcsmimg2.jpg'}
+        },
+        {
+          title: {imgName: 'columnList_yhh.png'},
+          secTitle: {data: '全民口碑推荐', color: '#50bdff'},
+          leftImg: {name: 'columnList_qmkbtjimg1.png'},
+          rightImg: {name: 'columnList_qmkbtjimg2.jpg'}
+        },
+        {
+          title: {imgName: 'columnList_wwsp.png'},
+          secTitle: {data: '粉丝都爱看', color: '#ff611d'},
+          leftImg: {name: 'columnList_fsdakimg1.png'},
+          rightImg: {name: 'columnList_fsdakimg2.jpg'}
+        }
+      ],
       heightScale: 1.033,
       styleScale: {columnHeightScale: 1.033}
     }
@@ -58,75 +154,240 @@ export default {
     border-top-left-radius: 12px;
 
     > div {
-      @include wh(100%, 33.333%);
+      @include wh(100%, 32.8%);
       display: flex;
     }
 
-    .leftFirst {
-      flex-wrap: wrap;
+    .leftContainer {
+      @include wh(50%, 100%);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+
+      .leftContainerTitle {
+        @include wh(100%, 22%);
+        background-position: left bottom;
+        background-size: contain;
+      }
+
+      .leftContainerImg {
+        @include wh(81%, 76%);
+        //background-color: #fdf5f2;
+        margin-left: 2%;
+        border-radius: 6px;
+
+        > span {
+          @include wh(100%, 30%);
+          display: flex;
+          align-items: center;
+          color: #fe4e50;
+          font: {
+            size: 0.8125rem;
+            weight: bold;
+          };
+          margin-left: 0.5rem;
+        }
+
+        > div {
+          @include wh(100%, 70%);
+          //@include bg("../../image/columnList_hshhimg.png");
+          background: {
+            size: contain;
+            position: center;
+          }
+        }
+      }
+    }
+
+    .leftFirstLeft {
+      //justify-content: space-around;
+      //align-items: center;
+
+      .leftContainerTitle {
+        @include bg("../../image/columnList_jhs.png");
+        background-size: 200%;
+      }
+
+      .leftContainerImg {
+        background-color: #fdf5f2;
+
+        > span {
+          color: #fe4e50;
+        }
+
+        > div {
+          @include bg("../../image/columnList_ppzkimg.png");
+        }
+      }
+    }
+
+    .leftFirstRight {
+      //justify-content: space-around;
+      align-items: flex-start;
+
+      .leftContainerImg {
+        background-color: #fcf9f3;
+
+        > span {
+          color: #ff7518;
+        }
+
+        > div {
+          @include bg("../../image/columnList_hshhimg.png");
+        }
+      }
+    }
+
+    .leftSecondLeft {
+      .leftContainerTitle {
+        @include bg("../../image/columnList_tqg.png");
+      }
+
+      .leftContainerImg {
+        background-color: #fff8fa;
+
+        > span {
+          color: #fa2a59;
+        }
+
+        > div {
+          @include bg("../../image/columnList_xsbjimg.png");
+        }
+      }
+    }
+
+    .leftSecondRight {
+      align-items: flex-start;
+
+      .leftContainerTitle {
+        @include bg("../../image/columnList_tttm.png");
+      }
+
+      .leftContainerImg {
+        background-color: #fff8f3;
+
+        > span {
+          color: #ff4c09;
+        }
+
+        > div {
+          @include bg("../../image/columnList_99byimg.png");
+        }
+      }
+    }
+
+    .leftThird {
+      //display: flex;
+      //flex-wrap: wrap;
+      //align-content: space-between;
+
+      .leftThirdTitle {
+        //@include wh(100%, 22%);
+        @include bg("../../image/columnList_mrhd.png");
+        //background: {
+        //  size: contain;
+        //  position: left bottom;
+        //};
+      }
+
+      .leftThirdLeft {
+        .leftThirdLeftBox {
+          > span {
+            color: #f9a500;
+          }
+
+          > div {
+            @include bg("../../image/columnList_wscddimg1.png");
+          }
+        }
+      }
+
+      .leftThirdRight {
+        //@include wh(50%, 76%);
+
+        > div {
+          @include wh(81%, 100%);
+          @include bg("../../image/columnList_wscddimg2.jpg");
+          background: {
+            size: contain;
+            position: center;
+          };
+        }
+      }
+    }
+  }
+
+  .containerB {
+    display: flex;
+    flex-wrap: wrap;
+    align-content: space-around;
+
+    .containerBTitle {
+      @include wh(100%, 22%);
+      background: {
+        repeat: no-repeat;
+        size: contain;
+        position: left bottom;
+      };
+    }
+
+    .containerBLeft {
+      @include wh(50%, 76%);
+      display: flex;
+      justify-content: space-around;
+
+      .containerBLeftBox {
+        @include wh(81%, 100%);
+        margin-left: 2%;
+
+        > span {
+          @include wh(100%, 30%);
+          display: flex;
+          align-items: center;
+          color: #f9a500;
+          font: {
+            size: 0.8125rem;
+            //weight: bold;
+          };
+          margin-left: 0.25rem;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+
+        > div {
+          @include wh(100%, 70%);
+          background: {
+            repeat: no-repeat;
+            size: contain;
+            position: center;
+          }
+        }
+      }
+    }
+
+    .containerBRight {
+      @include wh(50%, 76%);
 
       > div {
-        &:nth-child(1) {
-          @include wh(100%, 24%);
-          //background: red;
-          display: flex;
-          align-items: flex-end;
-
-          > div {
-            @include wh(100%, 67%);
-            @include bg("../../image/columnList_jhs.png");
-            background: {
-              size: cover;
-              position: center;
-            };
-          }
-        }
-
-        &:nth-child(2) {
-          @include wh(50%, 76%);
-          //background: blue;
-          display: flex;
-          justify-content: flex-end;
-
-          > div {
-            @include wh(85%, 100%);
-            background-color: #fdf5f2;
-            margin-right: 2px;
-            border-radius: 6px;
-
-            > span {
-              @include wh(100%, 30%);
-              display: flex;
-              align-items: center;
-              color: #fe4e50;
-              font: {
-                size: 0.8125rem;
-                weight: bold;
-              };
-              margin-left: 0.5rem;
-            }
-
-            > div {
-              @include wh(100%, 70%);
-              @include bg("../../image/columnList_ppzkimg.png");
-              background: {
-                size: contain;
-                position: center;
-              }
-            }
-          }
-        }
-
-        &:nth-child(3) {
-          @include wh(50%, 76%);
-          background: gold;
-        }
+        @include wh(81%, 100%);
+        background: {
+          repeat: no-repeat;
+          size: contain;
+          position: center;
+        };
       }
     }
   }
 
   .columnListRight {
     border-top-right-radius: 12px;
+
+    > div {
+      @include wh(100%, 32.8%);
+      display: flex;
+    }
   }
 }
 </style>
